18d24aa702619e2349b6ff7094e0edbf6b6f627d
Christian Samuel Seputra <stegenvak@gmail.com>
2023-03-29 13:51:05 +0700

    fin

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,23 @@
+<!-- navbar -->
+<nav class="navbar">
+    <a href="{{ route('home') }}" class="navbar-logo">
+        <img src="{{ url('asset/img/logo1.png') }}" alt="">
+    </a>
+
+    <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#menu">Menu</a>
+        <a href="#project">Projects</a>
+        <a href="#contact">Contact</a>
+    </div>
+
+    <div class="navbar-extra">
+        <a href="#" id="search">
+            <i data-feather="search"></i>
+        </a>
+        <a href="#" id="hamburger">
+            <i data-feather="menu"></i>
+        </a>
+    </div>
+</nav>
